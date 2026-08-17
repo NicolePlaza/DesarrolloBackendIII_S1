2e0748ca69a5c14100bdee7197ff2a3bccab66d1
camigmar <cam.gonzalez6@duocuc.cl>
2026-08-16 21:03:49 -0400

    Implementa Jobs de intereses y estados de cuenta anuales, agrega README y corrige test

--- src/test/java/com/banco/batch/BatchApplicationTests.java
+++ src/test/java/com/banco/batch/BatchApplicationTests.java
@@ -3,11 +3,11 @@ package com.banco.batch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "spring.batch.job.enabled=false")
 class BatchApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
